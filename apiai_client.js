884072ff92486640ef78124a165d0b7f5afb89ff
sriharan40 <sriharan40@gmail.com>
2016-11-26 23:43:09 +0530

    Update apiai_client.js

--- apiai_client.js
+++ apiai_client.js
@@ -27,17 +27,17 @@ function handleApiAiResponse(sender, response, callback) {
 		{
 		var messages = [{
 			"type":1,
-			"title":"Offers List.",
+			"title":"Youtube offers list.",
 			"subtitle":"Offers",
 			"imageUrl":"http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F05%2Fnexus2cee_YouTube_thumb1.png&w=150&h=150&zc=3",
 			"buttons":[
               {
-                "postback":"https://petersfancybrownhats.com",
-                "text":"View Website"
+                "postback":"https://goo.gl/sIZCze",
+                "text":"Youtube 1 day"
               },
               {
-                "postback":"https://petersfancybrownhats.com",
-                "text":"View Website1"
+                "postback":"https://goo.gl/6eFDBP",
+                "text":"Facebook 1 hr"
               }              
             ]			
 			}]
