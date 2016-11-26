fb3df2e1cef1a02c16dbfe626aadb1106bc188a5
sriharan40 <sriharan40@gmail.com>
2016-11-26 23:37:22 +0530

    Update apiai_client.js

--- apiai_client.js
+++ apiai_client.js
@@ -29,7 +29,17 @@ function handleApiAiResponse(sender, response, callback) {
 			"type":1,
 			"title":"Offers List.",
 			"subtitle":"Offers",
-			"imageUrl":"http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F05%2Fnexus2cee_YouTube_thumb1.png&w=150&h=150&zc=3"
+			"imageUrl":"http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F05%2Fnexus2cee_YouTube_thumb1.png&w=150&h=150&zc=3",
+			"buttons":[
+              {
+                "postback":"https://petersfancybrownhats.com",
+                "text":"View Website"
+              },
+              {
+                "postback":"https://petersfancybrownhats.com",
+                "text":"View Website1"
+              }              
+            ]			
 			}]
 		}
 		
