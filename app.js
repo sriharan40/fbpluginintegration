4d83aeda8b008f4bef70636312c5a5ec359ec371
sriharan40 <sriharan40@gmail.com>
2016-11-26 13:33:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,234 +1,112 @@
+/* jshint node: true */
 'use strict';
 
 const apiai = require('apiai');
 const express = require('express');
 const bodyParser = require('body-parser');
-const uuid = require('node-uuid');
+const uuid = require('uuid');
 const request = require('request');
-const JSONbig = require('json-bigint');
-const async = require('async');
+const JSONbigint = require('json-bigint');
 const fbClient = require('./fb_client');
 const apiaiClient = require('./apiai_client');
 
-const REST_PORT = (process.env.PORT || 5000);
+const WEBSERVER_PORT = (process.env.PORT || 5000);
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
 const APIAI_LANG = process.env.APIAI_LANG || 'en';
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
 
+// Create apiai service
 const apiAiService = apiai(APIAI_ACCESS_TOKEN, {language: APIAI_LANG, requestSource: "fb"});
 const sessionIds = new Map();
 
-function processEvent(event) {
+/**
+ * Process an incoming event from facebook
+ *
+ * @param event
+ */
+function processFacebookEvent(event) {
+
+    // Get sender id
     var sender = event.sender.id.toString();
-    console.log("Sender is : " + sender);
 
-    if ((event.message && event.message.text) || (event.postback && event.postback.payload)) {
-        var text = event.message ? event.message.text : event.postback.payload;
-        // Handle a text message from this sender
+    var text = "";
+    if (event.message && event.message.text) {
+        text = event.message.text;
+    } else if (event.postback && event.postback.payload) {
+        text = event.postback.payload;
+    }
 
+    if (text) {
+
+        // Store a new session for this sender
         if (!sessionIds.has(sender)) {
             sessionIds.set(sender, uuid.v1());
         }
 
-        console.log("Text", text);
-
-        let apiaiRequest = apiAiService.textRequest(text,
-            {
-                sessionId: sessionIds.get(sender)
-            });
-
-        apiaiRequest.on('response', (response) => {
-            if (isDefined(response.result)) {
-                let responseText = response.result.fulfillment.speech;
-                let responseData = response.result.fulfillment.data;
-                let action = response.result.action;
-
-                if (isDefined(responseData) && isDefined(responseData.facebook)) {
-                    if (!Array.isArray(responseData.facebook)) {
-                        try {
-                            console.log('Response as formatted message');
-                            sendFBMessage(sender, responseData.facebook);
-                        } catch (err) {
-                            sendFBMessage(sender, {text: err.message});
-                        }
-                    } else {
-                        async.eachSeries(responseData.facebook, (facebookMessage, callback) => {
-                            try {
-                                if (facebookMessage.sender_action) {
-                                    console.log('Response as sender action');
-                                    sendFBSenderAction(sender, facebookMessage.sender_action, callback);
+        fbClient.userInfoRequest(sender)
+            .then(userInfoStr=> {
+                // Initialize userInto
+                var userInfo = {first_name: "friend"};
+                try {
+                    userInfo = JSON.parse(userInfoStr);
+                } catch (err) {
+                    console.error("Could not parse userInfoStr: %s", userInfoStr)
+                }
+                var apiaiRequest = apiAiService.textRequest(text,
+                    {
+                        sessionId: sessionIds.get(sender),
+                        contexts: [
+                            {
+                                name: "generic",
+                                parameters: {
+                                    facebook_user: userInfo.first_name
                                 }
-                                else {
-                                    console.log('Response as formatted message');
-                                    sendFBMessage(sender, facebookMessage, callback);
-                                }
-                            } catch (err) {
-                                sendFBMessage(sender, {text: err.message}, callback);
                             }
-                        });
-                    }
-                } else if (isDefined(responseText)) {
-                    console.log('Response as text message');
-                    // facebook API limit for text length is 320,
-                    // so we must split message if needed
-                    var splittedText = splitResponse(responseText);
-
-                    async.eachSeries(splittedText, (textPart, callback) => {
-                        sendFBMessage(sender, {text: textPart}, callback);
+                        ]
                     });
-                }
 
-            }
+                // Handle response
+                apiaiRequest.on('response', response => {
+                    apiaiClient.handleApiAiResponse(sender, response);
+                });
+                apiaiRequest.on('error', error => console.error(error));
+                apiaiRequest.end();
+
+            }).catch(err => {
+            console.error(err);
         });
-
-        apiaiRequest.on('error', (error) => console.error(error));
-        apiaiRequest.end();
     }
 }
 
-function splitResponse(str) {
-    if (str.length <= 320) {
-        return [str];
-    }
 
-    return chunkString(str, 300);
-}
-
-function chunkString(s, len) {
-    var curr = len, prev = 0;
-
-    var output = [];
-
-    while (s[curr]) {
-        if (s[curr++] == ' ') {
-            output.push(s.substring(prev, curr));
-            prev = curr;
-            curr += len;
-        }
-        else {
-            var currReverse = curr;
-            do {
-                if (s.substring(currReverse - 1, currReverse) == ' ') {
-                    output.push(s.substring(prev, currReverse));
-                    prev = currReverse;
-                    curr = currReverse + len;
-                    break;
-                }
-                currReverse--;
-            } while (currReverse > prev)
-        }
-    }
-    output.push(s.substr(prev));
-    return output;
-}
-
-function sendFBMessage(sender, messageData, callback) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: FB_PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: sender},
-            message: messageData
-        }
-    }, (error, response, body) => {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-
-        if (callback) {
-            callback();
-        }
-    });
-}
-
-function sendFBSenderAction(sender, action, callback) {
-    setTimeout(() => {
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {access_token: FB_PAGE_ACCESS_TOKEN},
-            method: 'POST',
-            json: {
-                recipient: {id: sender},
-                sender_action: action
-            }
-        }, (error, response, body) => {
-            if (error) {
-                console.log('Error sending action: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-            if (callback) {
-                callback();
-            }
-        });
-    }, 1000);
-}
-
-function doSubscribeRequest() {
-    request({
-            method: 'POST',
-            uri: "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=" + FB_PAGE_ACCESS_TOKEN
-        },
-        (error, response, body) => {
-            if (error) {
-                console.error('Error while subscription: ', error);
-            } else {
-                console.log('Subscription result: ', response.body);
-            }
-        });
-}
-
-function isDefined(obj) {
-    if (typeof obj == 'undefined') {
-        return false;
-    }
-
-    if (!obj) {
-        return false;
-    }
-
-    return obj != null;
-}
-
+// Create Express Server
 const app = express();
 
+// Enable json parsing
 app.use(bodyParser.text({type: 'application/json'}));
 
-app.get('/webhook/', (req, res) => {
+// Main webhook endpoint configuration
+app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] == FB_VERIFY_TOKEN) {
         res.send(req.query['hub.challenge']);
 
-        setTimeout(() => {
-            doSubscribeRequest();
+        setTimeout(function () {
+            fbClient.subscribeRequest();
         }, 3000);
     } else {
         res.send('Error, wrong validation token');
     }
 });
 
-app.post('/webhook/', (req, res) => {
+app.post('/webhook/', function (req, res) {
     try {
-        var data = JSONbig.parse(req.body);
+        // Load data
+        var data = JSONbigint.parse(req.body);
 
-        if (data.entry) {
-            let entries = data.entry;
-            entries.forEach((entry) => {
-                let messaging_events = entry.messaging;
-                if (messaging_events) {
-                    messaging_events.forEach((event) => {
-                        if (event.message && !event.message.is_echo ||
-                            event.postback && event.postback.payload) {
-                            processEvent(event);
-                        }
-                    });
-                }
-            });
+        for (var i = 0; i < data.entry[0].messaging.length; i++) {
+            var event = data.entry[0].messaging[i];
+            processFacebookEvent(event);
         }
-
         return res.status(200).json({
             status: "ok"
         });
@@ -241,11 +119,30 @@ app.post('/webhook/', (req, res) => {
 
 });
 
-app.listen(REST_PORT, () => {
+// For testing purposes (testing api.ai-like callback posts)
+app.post('/test-apiai-callback/', function (req, res) {
+    try {
+        // Load data
+        var data = JSONbigint.parse(req.body);
+
+        // Forward data to Api.AI
+        apiaiClient.handleApiAiResponse('1217505768324329', data);
+
+        return res.status(200).json({
+            status: "ok"
+        });
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
+    }
+});
+
+// Start web server
+app.listen(REST_PORT, function () {
     console.log('Rest service ready on port ' + REST_PORT);
 });
 
-//doSubscribeRequest();
-   //let fb client subscribe it
-    // Subscribe
+// Subscribe
 fbClient.subscribeRequest();
