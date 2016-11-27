55c24cf66d3f19ecd1f99c6b6532a30a74239303
sriharan40 <sriharan40@gmail.com>
2016-11-27 19:59:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,58 @@ const sessionIds = new Map();
  * @param event
  */
  
+function sendResponse(sender, message, callback) {
+
+    switch (message.type) {
+        case 0: // message text
+            fbClient.sendSplitMessages(sender, message.speech);
+            break;
+        case 1: // card
+            var buttons = [];
+            if (misc.isDefined(message.buttons)) {
+                async.eachSeries(message.buttons, (button, innerCallback) => {
+                    var payload = button.postback || button.text;
+                    if (payload.match(/http(s)?\:\/\/.*$/)) {
+                        buttons.push({
+                            type: "web_url",
+                            title: button.text,
+                            url: payload
+                        });
+                    } else {
+                        buttons.push({
+                            type: "postback",
+                            title: button.text,
+                            payload: payload
+                        });
+                    }
+
+                    console.info(buttons);
+                    innerCallback();
+                });
+            }
+            fbClient.sendCardMessage(sender, message.title, message.subtitle, message.imageUrl, buttons);
+            break;
+        case 2: // quick replies
+            if (misc.isDefined(message.replies)) {
+                var questions = [];
+                async.eachSeries(message.replies, (reply, innerCallback) => {
+                    questions.push({text: reply, payload: reply});
+                    innerCallback();
+                });
+                fbClient.sendQuickReplyQuestion(sender, message.title, questions);
+            }
+            break;
+        case 3: // image
+            if (misc.isDefined(message.imageUrl)) {
+                fbClient.sendImageMessage(sender, message.imageUrl);
+            }
+            break;
+    }
+    if (callback) {
+        callback();
+    }
+}
+ 
 function messageformat(action)
 {	
 	if(action == "showOfferOptionsToUser")
