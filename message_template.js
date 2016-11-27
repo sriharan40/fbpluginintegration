62f808c86096f013bf8dbdefc582dd5ca29e314c
sriharan40 <sriharan40@gmail.com>
2016-11-28 00:44:09 +0530

    Update message_template.js

--- message_template.js
+++ message_template.js
@@ -54,17 +54,10 @@ connection.query('select * from offers', function(err, rows, fields) {
 			"title":"Top Pick Offers today.",
 			"subtitle":"Vamos",
  			"imageUrl":"http://images.hardwarezone.com/upload/files/2013/11/6891939f6d.jpg",
-			"buttons":[
-			{
-				"postback":"https://goo.gl/6eFDBP",
-				"text":"Facebook 1 hr"
-			},
-			{
-				"postback":"https://goo.gl/sIZCze",
-				"text":"Youtube 1 day"
-			}
-			]
+			"buttons":[{"postback":"https://goo.gl/6eFDBP","text":"Facebook 1 hr"},{"postback":"https://goo.gl/sIZCze","text":"Youtube 1 day"},{"postback":"https://goo.gl/G8x0Rq","text":"Clash of Clans"}]
 			}]
+
+console.log("Message:"+messages);			
 
 }		
 		
@@ -91,4 +84,4 @@ return messages;
 // Export module functions
 module.exports = {
     messageformat: messageformat,
-};	
+};
