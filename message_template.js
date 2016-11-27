9b0959694cf0edfd94b4ffdb0400ff754c7034f5
sriharan40 <sriharan40@gmail.com>
2016-11-27 23:59:11 +0530

    Update message_template.js

--- message_template.js
+++ message_template.js
@@ -6,26 +6,58 @@ const misc = require('./misc');
 const async = require('async');
 
 function messageformat(action)
-{	
-	if(action == "showOfferOptionsToUser")
-		{
+{
+var db_config = {
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: process.env.db_user,
+    password: process.env.db_pass,
+    database: 'heroku_a0067bd7c868fc0'
+};
+
+
+var connection;
+
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+	
+if(action == "showOfferOptionsToUser")
+{
+var arr1 = [];
+
+connection.query('select * from offers', function(err, rows, fields) {
+    if (err) throw err; 
+    for (var i in rows) {
+         arr1.push({
+            "type":"web_url",
+            "url":rows[i].description,
+            "title":rows[i].offer_name
+          })
+    }			
 		var messages = [{
 			"type":1,
 			"title":"Top Pick Offers today.",
 			"subtitle":"Vamos",
  			"imageUrl":"http://images.hardwarezone.com/upload/files/2013/11/6891939f6d.jpg",
-			"buttons":[
-              {
-                "postback":"https://goo.gl/sIZCze",
-                "text":"Youtube 1 day"
-              },
-              {
-                "postback":"https://goo.gl/6eFDBP",
-                "text":"Facebook 1 hr"
-              }              
-            ]			
+			"buttons": arr1
 			}]
-		}
+			
+		});
+}		
 		
 		if(action == "UserAcceptance")
 		{
