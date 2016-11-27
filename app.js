bb404e73ece30a32a5df33bce67a8d67aa0beec1
sriharan40 <sriharan40@gmail.com>
2016-11-27 19:38:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,47 @@ const sessionIds = new Map();
  * @param event
  */
  
+function messageformat(action)
+{	
+	if(action == "showOfferOptionsToUser")
+		{
+		var messages = [{
+			"type":1,
+			"title":"Youtube offers list.",
+			"subtitle":"Offers",
+			"imageUrl":"http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F05%2Fnexus2cee_YouTube_thumb1.png&w=150&h=150&zc=3",
+			"buttons":[
+              {
+                "postback":"https://goo.gl/sIZCze",
+                "text":"Youtube 1 day"
+              },
+              {
+                "postback":"https://goo.gl/6eFDBP",
+                "text":"Facebook 1 hr"
+              }              
+            ]			
+			}]
+		}
+		
+		if(action == "UserAcceptance")
+		{
+		var messages = [{
+			"type":0,
+			"speech":"Thanks for your response."
+			}]			
+		}
+
+		if(action == "Surprisetalk")
+		{
+		var messages = [{
+			"type":0,
+			"speech":"Welcome to bot chat."
+			}]			
+		}
+		
+return messages;
+}		
+ 
 function jsonvalue(json,string)
 {	
 for (var name in json) {
@@ -88,7 +129,7 @@ if(name6 == string)
  }   
  }
  }   
-   }	
+ }	
  }	
 return ref; 
 }
@@ -102,7 +143,7 @@ function processFacebookEvent(event) {
 	
 	if(ref)
 	{
-	var messages = messag.messageformat(ref);
+	var messages = messageformat(ref);
 
 	console.log("message:"+messages);
 	
