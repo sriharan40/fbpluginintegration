fe97d007818443c9ff055e8d677d3248f5be005c
sriharan40 <sriharan40@gmail.com>
2016-11-27 21:42:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const fbClient = require('./fb_client');
 const apiaiClient = require('./apiai_client');
 const misc = require('./misc');
 const async = require('async');
-const messag = require('./message');
+const messag = require('./message_template');
 
 const REST_PORT = (process.env.PORT || 5000);
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
@@ -79,47 +79,6 @@ function sendResponse(sender, message, callback) {
         callback();
     }
 }
- 
-function messageformat(action)
-{	
-	if(action == "showOfferOptionsToUser")
-		{
-		var messages = [{
-			"type":1,
-			"title":"Youtube offers list.",
-			"subtitle":"Offers",
-			"imageUrl":"http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F05%2Fnexus2cee_YouTube_thumb1.png&w=150&h=150&zc=3",
-			"buttons":[
-              {
-                "postback":"https://goo.gl/sIZCze",
-                "text":"Youtube 1 day"
-              },
-              {
-                "postback":"https://goo.gl/6eFDBP",
-                "text":"Facebook 1 hr"
-              }              
-            ]			
-			}]
-		}
-		
-		if(action == "UserAcceptance")
-		{
-		var messages = [{
-			"type":0,
-			"speech":"Thanks for your response."
-			}]			
-		}
-
-		if(action == "surprisetalk")
-		{
-		var messages = [{
-			"type":0,
-			"speech":"Welcome to bot chat."
-			}]			
-		}
-		
-return messages;
-}		
  
 function jsonvalue(json,string)
 {	
@@ -195,7 +154,7 @@ function processFacebookEvent(event) {
 	
 	if(ref)
 	{
-	var messages = messageformat(ref);
+	var messages = messag.messageformat(ref);
 	
 if(messages)
 {		
