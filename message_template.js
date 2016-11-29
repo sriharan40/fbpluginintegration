e84173ea7e9924f26539ad77ce2c2c2724b3b277
sriharan40 <sriharan40@gmail.com>
2016-11-30 01:07:21 +0530

    Update message_template.js

--- message_template.js
+++ message_template.js
@@ -3,9 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-var sync = require('synchronize');
-var globals = require('globals');
-//var mysql = require('mysql-libmysqlclient') ;
+const apiaiClient = require('./apiai_client');
 //const fbClient = require('./fb_client');
 //const misc = require('./misc');
 var async = require('async');
@@ -45,119 +43,33 @@ var connection;
         }
     });
 
-//sync.await(connection.query('select * from offers', function(err, rows, fields) {
-
-var messages12 = "Text";
-
-var someVar = [];
-
 connection.query("select * from offers", function(err, rows){
   if(err) {
     throw err;
   } else {
-    setValue(rows);
-  }
-});
+	  
+for (var i in rows) {
+	 arr1.push({
+		"postback":rows[i].description,
+		"text":rows[i].offer_name
+	  })
+}	  
 
-function setValue(value) {
-  someVar = value;
-//  console.log(someVar);
+var messages12 = [{
+	"type":1,
+	"title":"Top Pick Offers today.",
+	"subtitle":"Vamos",
+	"imageUrl":"http://images.hardwarezone.com/upload/files/2013/11/6891939f6d.jpg",
+	"buttons":arr1
+	//[{"postback":"https://goo.gl/6eFDBP","text":"Facebook 1 hr"},{"postback":"https://goo.gl/sIZCze","text":"Youtube 1 day"},{"postback":"https://goo.gl/G8x0Rq","text":"Clash of Clans"}]
+	}]
+
+console.log("Message1:"+JSON.stringify(messages12));
+	  
+apiaiClient.getmessage(messages12);
 }
-	
-  console.log(someVar);
-	
-/* function getTopOffers(callback) {
-    var ret;
-	connection.query("SELECT * from offers", function(err, rows, fields) {
-        if (err) {
-            // You must `return` in this branch to avoid using callback twice.
-            return callback(err);
-        }
-
-        // Do something with `rows` and `fields` and assign a value to ret.
-	ret = rows;
-        return callback(null, ret);
-    });
-}
-
-
-function handleResult(err, result) {
-console.log("In handle result");	
-    if (err) {
-        // Just an example. You may want to do something with the error.
-        console.error(err.stack || err.message);
-
-        // You should return in this branch, since there is no result to use
-        // later and that could cause an exception.
-        return;
-    }
-console.log("result:"+JSON.stringify(result));	
-return;
-    // All your logic with the result.
-}
-
-var offer_data = getTopOffers(handleResult);
-	
-console.log("result12:"+JSON.stringify(offer_data));	
-
-	console.log("result13:"+JSON.stringify(getTopOffers(handleResult)));	
-
-/* function getTopoffers() {
-	
-var rows = connection.query('select * FROM offers', function(err, result) {
-if(err)
-{
-throw err;
-}
-else
-{
-//console.log(result);		
-}	
-return result;
-});
-	
-console.log("Rows:"+JSON.stringify(rows));
-	
-	//{
-        //    if (err) {
-        //        callback(err, null);
-        //    } else 
-	for (var i in rows) {
-         arr1.push({
-            "postback":rows[i].description,
-            "text":rows[i].offer_name
-          })
-	}
-
-		messages12 = [{
-			"type":1,
-			"title":"Top Pick Offers today.",
-			"subtitle":"Vamos",
- 			"imageUrl":"http://images.hardwarezone.com/upload/files/2013/11/6891939f6d.jpg",
-			"buttons":arr1
-			//[{"postback":"https://goo.gl/6eFDBP","text":"Facebook 1 hr"},{"postback":"https://goo.gl/sIZCze","text":"Youtube 1 day"},{"postback":"https://goo.gl/G8x0Rq","text":"Clash of Clans"}]
-			}]
-
-		console.log("Message1:"+JSON.stringify(messages12));
-			
-                return messages12;
-	        //callback(null, messages12);
-  //      });
-	
-}		
-
-//messages12 = getTopoffers(); 
-	
-	//{ //This is the final callback
-
-// = content;
-
-console.log("Message2:"+JSON.stringify(getTopoffers()));
-
-//});
-
-return getTopoffers();
-*/
+  
+});	
 	
 }								
 		
