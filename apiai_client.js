5cc5cbbba495fb65fb35f11b16081ea3e0f4617e
sriharan40 <sriharan40@gmail.com>
2016-11-30 01:24:24 +0530

    Update apiai_client.js

--- apiai_client.js
+++ apiai_client.js
@@ -14,29 +14,7 @@ const messag = require('./message_template');
  * @param response Response from API.AI.
  * @param callback Callback function.
  */
- 
-function getmessage(messages)
-{
-if(messages)
-{
-// Adding delay between responses
-var i = 0;
-async.whilst(
-	function () {
-		return i <= messages.length - 1;
-	},
-	function (innerCallback) {
-		sendResponse(sender, messages[i], function () {
-			setTimeout(function () {
-				i++;
-				innerCallback();
-			}, 1000);
-		})
-	}, callback);
-}
-
-}
-		
+ 	
 function handleApiAiResponse(sender, response, callback) {
 
     // Do we have a result?
@@ -48,7 +26,7 @@ function handleApiAiResponse(sender, response, callback) {
 		
 		var action = response.result.action;
 
-		var messages = messag.messageformat(action);
+		var messages = messag.messageformat(action, sender);
     }
 }
 
@@ -115,6 +93,5 @@ function sendResponse(sender, message, callback) {
 // Expoer module functions
 module.exports = {
     handleApiAiResponse: handleApiAiResponse,
-	sendResponse: sendResponse,
-	getmessage: getmessage
+	sendResponse: sendResponse
 };
