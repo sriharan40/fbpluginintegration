feee5f942d38292a0656024dbc5573026b4f7385
sriharan40 <sriharan40@gmail.com>
2016-11-30 23:41:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const JSONbigint = require('json-bigint');
 const fbClient = require('./fb_client');
 const apiaiClient = require('./apiai_client');
 const messag = require('./message_template');
-const opener = require("opener");
+//const opener = require("opener");
+const opener = require('opn');
 const misc = require('./misc');
 const async = require('async');
 
