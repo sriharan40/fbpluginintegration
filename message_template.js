79789ecdb6dab8618d537740a18b9e8e7fa44df9
sriharan40 <sriharan40@gmail.com>
2016-12-07 20:52:08 +0530

    Update message_template.js

--- message_template.js
+++ message_template.js
@@ -28,10 +28,10 @@ function messageformat(action,sender,response)
 {
 var arr1 = [];
 var messages12 = "";
+
+switch (action) {
 	
-if(action == "showOfferOptionsToUser" || action == "surprisetalk")
-{
-/* FB.AppEvents.logEvent("Offer Ref from Deeplink"); */
+case "showOfferOptionsToUser":
 var db_config = {
     host: 'us-cdbr-iron-east-04.cleardb.net',
     user: process.env.db_user,
@@ -91,11 +91,75 @@ console.log("Message1:"+JSON.stringify(messages12));
 
 }
 
-});	
-	
-}								
-		
-else if(action == "UserAcceptance")
+});
+break;
+
+case "surprisetalk":
+var db_config = {
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: process.env.db_user,
+    password: process.env.db_pass,
+    database: 'heroku_a0067bd7c868fc0'
+};
+
+
+var connection;
+
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+
+connection.query("select * from offers", function(err, rows){
+  if(err) {
+    throw err;
+  } else {
+	  
+for (var i in rows) {
+	 arr1.push({
+		"postback":rows[i].description,
+		"text":rows[i].offer_name
+	  })
+}	  
+
+connection.query('select * from category', function(err, rows12) {
+
+var messages12 = [{
+	"type":1,
+	"title":rows12[0].title,
+	"subtitle":rows12[0].sub_title,
+	"imageUrl":rows12[0].img_url,
+	"buttons":arr1
+	}]
+
+console.log("Message1:"+JSON.stringify(messages12));
+
+ fbClient.sendCardMessage(sender, messages12.title, messages12.subtitle, messages12.imageUrl, messages12.buttons);
+//apiaiClient.handleMessages(messages12,sender,"");
+	  
+});  
+
+}
+
+});
+break;
+
+}
+
+if(action == "UserAcceptance")
 {
 /* FB.AppEvents.logEvent("UserAcceptance"); */	
 var messages = [{
